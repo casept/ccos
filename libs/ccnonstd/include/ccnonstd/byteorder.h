62fa108758ecfcf276d389823148898f81e2c0bc
casept <davids.paskevics@gmail.com>
2021-09-05 21:59:41 +0200

    [ccnonstd] Tweak byteorder

--- libs/ccnonstd/include/ccnonstd/byteorder.h
+++ libs/ccnonstd/include/ccnonstd/byteorder.h
@@ -9,16 +9,31 @@
 /// Return `false` for big endian, `true` for little endian.
 ///
 /// Return value on mixed-endian architectures is undefined.
-inline bool byteorder(void);
+bool byteorder(void);
 
 /// Read given big-endian byte buffer into a `uint16_t` of native endianness.
-inline uint16_t byteorder_be_to_u16_native(const uint8_t buf[2]);
+uint16_t byteorder_be_to_u16_native(const uint8_t buf[2]);
 /// Read given little-endian byte buffer into a `uint16_t` of native endianness.
-inline uint16_t byteorder_le_to_u16_native(const uint8_t buf[2]);
+uint16_t byteorder_le_to_u16_native(const uint8_t buf[2]);
 /// Wrapper for big-endian and little-endian to native functions
 /// which calls the big-endian variant if first argument is `false`,
 /// or the little-endian variant if first argument is `true`.
-inline uint16_t byteorder_to_u16_native(bool endianness, const uint8_t buf[2]);
+uint16_t byteorder_to_u16_native(bool endianness, const uint8_t buf[2]);
 
 /// Read given big-endian byte buffer into a `uint32_t` of native endianness.
-inline uint32_t byteorder_be_to_u32_native(const uint8_t buf[4]);
+uint32_t byteorder_be_to_u32_native(const uint8_t buf[4]);
+/// Read given little-endian byte buffer into a `uint32_t` of native endianness.
+uint32_t byteorder_le_to_u32_native(const uint8_t buf[4]);
+/// Wrapper for big-endian and little-endian to native functions
+/// which calls the big-endian variant if first argument is `false`,
+/// or the little-endian variant if first argument is `true`.
+uint32_t byteorder_to_u32_native(bool endianness, const uint8_t buf[4]);
+
+/// Read given big-endian byte buffer into a `uint32_t` of native endianness.
+uint64_t byteorder_be_to_u64_native(const uint8_t buf[8]);
+/// Read given little-endian byte buffer into a `uint32_t` of native endianness.
+uint64_t byteorder_le_to_u64_native(const uint8_t buf[8]);
+/// Wrapper for big-endian and little-endian to native functions
+/// which calls the big-endian variant if first argument is `false`,
+/// or the little-endian variant if first argument is `true`.
+uint64_t byteorder_to_u64_native(bool endianness, const uint8_t buf[8]);
--- libs/ccnonstd/src/byteorder.c
+++ libs/ccnonstd/src/byteorder.c
@@ -1,32 +1,32 @@
 #include "../include/ccnonstd/byteorder.h"
 
+#include <stddef.h>
 #include <stdint.h>
 
-inline bool byteorder(void) {
-    uint8_t swaptest[2] = {1, 0};
+bool byteorder(void) {
+    // Alignment required for cast to be always legal
+    uint8_t __attribute__((aligned(2))) swaptest[2] = {1, 0};
     if (*(uint16_t*)swaptest == 1) {
         return true;
     }
     return false;
 }
 
-inline uint16_t byteorder_be_to_u16_native(const uint8_t buf[2]) {
-    if (byteorder()) {
-        return (((uint16_t)buf[2]) << 8) | ((uint16_t)buf[1]);
-    } else {
-        return (((uint16_t)buf[1]) << 8) | ((uint16_t)buf[2]);
-    }
+uint16_t byteorder_be_to_u16_native(const uint8_t buf[2]) {
+    uint16_t x;
+    x = ((uint16_t)buf[0]) << 8;
+    x |= ((uint16_t)buf[1]);
+    return x;
 }
 
-inline uint16_t byteorder_le_to_u16_native(const uint8_t buf[2]) {
-    if (byteorder()) {
-        return (((uint16_t)buf[1]) << 8) | ((uint16_t)buf[2]);
-    } else {
-        return (((uint16_t)buf[2]) << 8) | ((uint16_t)buf[1]);
-    }
+uint16_t byteorder_le_to_u16_native(const uint8_t buf[2]) {
+    uint16_t x;
+    x = ((uint16_t)buf[1]) << 8;
+    x |= ((uint16_t)buf[0]);
+    return x;
 }
 
-inline uint16_t byteorder_to_u16_native(bool endianness, const uint8_t buf[2]) {
+uint16_t byteorder_to_u16_native(bool endianness, const uint8_t buf[2]) {
     if (endianness) {
         return byteorder_le_to_u16_native(buf);
     } else {
@@ -34,10 +34,62 @@ inline uint16_t byteorder_to_u16_native(bool endianness, const uint8_t buf[2]) {
     }
 }
 
-inline uint32_t byteorder_be_to_u32_native(const uint8_t buf[4]) {
-    if (byteorder()) {
-        return (((uint32_t)buf[4]) << 24) | (((uint32_t)buf[3]) << 16) | (((uint32_t)buf[2]) << 8) | ((uint32_t)buf[1]);
+uint32_t byteorder_be_to_u32_native(const uint8_t buf[4]) {
+    uint32_t x;
+    x = ((uint32_t)buf[0]) << 24;
+    x |= ((uint32_t)buf[1]) << 16;
+    x |= ((uint32_t)buf[2]) << 8;
+    x |= ((uint32_t)buf[3]);
+    return x;
+}
+
+uint32_t byteorder_le_to_u32_native(const uint8_t buf[4]) {
+    uint32_t x;
+    x = ((uint32_t)buf[3]) << 24;
+    x |= ((uint32_t)buf[2]) << 16;
+    x |= ((uint32_t)buf[1]) << 8;
+    x |= ((uint32_t)buf[0]);
+    return x;
+}
+
+uint32_t byteorder_to_u32_native(bool endianness, const uint8_t buf[4]) {
+    if (endianness) {
+        return byteorder_le_to_u32_native(buf);
     } else {
-        return (((uint32_t)buf[1]) << 24) | (((uint32_t)buf[2]) << 16) | (((uint32_t)buf[3]) << 8) | ((uint32_t)buf[4]);
+        return byteorder_be_to_u32_native(buf);
+    }
+}
+
+uint64_t byteorder_be_to_u64_native(const uint8_t buf[8]) {
+    uint64_t x;
+    x = ((uint64_t)buf[0]) << 56;
+    x |= ((uint64_t)buf[1]) << 48;
+    x |= ((uint64_t)buf[2]) << 40;
+    x |= ((uint64_t)buf[3]) << 32;
+    x |= ((uint64_t)buf[4]) << 24;
+    x |= ((uint64_t)buf[5]) << 16;
+    x |= ((uint64_t)buf[6]) << 8;
+    x |= ((uint64_t)buf[7]);
+    return x;
+}
+
+uint64_t byteorder_le_to_u64_native(const uint8_t buf[8]) {
+    uint64_t x;
+    x = ((uint64_t)buf[7]) << 56;
+    x |= ((uint64_t)buf[6]) << 48;
+    x |= ((uint64_t)buf[5]) << 40;
+    x |= ((uint64_t)buf[4]) << 32;
+    x |= ((uint64_t)buf[3]) << 24;
+    x |= ((uint64_t)buf[2]) << 16;
+    x |= ((uint64_t)buf[1]) << 8;
+    x |= ((uint64_t)buf[0]);
+    return x;
+}
+
+uint64_t byteorder_to_u64_native(bool endianness, const uint8_t buf[8]) {
+    if (endianness) {
+        return byteorder_le_to_u64_native(buf);
+    } else {
+        return byteorder_be_to_u64_native(buf);
     }
 }
