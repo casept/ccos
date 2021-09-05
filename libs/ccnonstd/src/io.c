ead84f99a6dae80887ec8126d5339cd8747adfd0
casept <davids.paskevics@gmail.com>
2021-09-05 22:07:36 +0200

    [ccnonstd] Implement formatting of integers

--- libs/ccnonstd/src/io.c
+++ libs/ccnonstd/src/io.c
@@ -1,7 +1,58 @@
 #include "../include/ccnonstd/io.h"
 
 #include <stdarg.h>
+#include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void vprintf_uint_to_base(const vprintf_sink sink, unsigned int uint, unsigned int base) {
+    const char* digits = "0123456789ABCDEF";
+
+    // 0 is a special case
+    if (uint == 0) {
+        sink('0');
+        return;
+    }
+
+    // Would be large enough for even a hypothetical machine with 128-bit unsigned int
+    const size_t buf_size = 64;
+    char buf[buf_size];
+    memset(buf, '\0', buf_size * sizeof(char));
+
+    for (size_t i = 0; i < buf_size && uint != 0; i++) {
+        const size_t digit = (size_t)(uint % base);
+        buf[i] = digits[digit];
+        uint /= base;
+    }
+
+    if (uint != 0) {
+        const size_t digit = (size_t)uint;
+        sink(digits[digit]);
+    }
+
+    // Order of digits is reversed
+    size_t i = buf_size;
+    while (true) {
+        if (buf[i] != '\0') {
+            sink(buf[i]);
+        }
+        if (i == 0) {
+            break;
+        }
+        i--;
+    }
+}
+
+static void vprintf_sint_to_decimal(const vprintf_sink sink, int sint) {
+    if (sint < 0) {
+        sink('-');
+    }
+
+    const unsigned int uint = (unsigned int)abs(sint);
+    vprintf_uint_to_base(sink, uint, 10);
+}
 
 int vprintf_generic(const vprintf_sink sink, const char* format, va_list vlist) {
     size_t i = 0;
@@ -14,6 +65,8 @@ int vprintf_generic(const vprintf_sink sink, const char* format, va_list vlist) 
             // Predeclared because vars can't be declared in switch
             char* cc;
             char ch;
+            unsigned int uint;
+            int sint;
             switch (fmt) {
                 case 's':
                     cc = va_arg(vlist, char*);
@@ -26,6 +79,23 @@ int vprintf_generic(const vprintf_sink sink, const char* format, va_list vlist) 
                     // Is promoted to an int and then immediately demoted back to char... facepalm
                     ch = va_arg(vlist, int);
                     sink(ch);
+                    break;
+                case 'u':
+                    uint = va_arg(vlist, unsigned int);
+                    vprintf_uint_to_base(sink, uint, 10);
+                    break;
+                case 'x':
+                    uint = va_arg(vlist, unsigned int);
+                    vprintf_uint_to_base(sink, uint, 16);
+                    break;
+                case 'o':
+                    uint = va_arg(vlist, unsigned int);
+                    vprintf_uint_to_base(sink, uint, 8);
+                    break;
+                case 'd':
+                case 'i':
+                    sint = va_arg(vlist, int);
+                    vprintf_sint_to_decimal(sink, sint);
                     break;
                 default:
                     return -1;
