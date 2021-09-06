fd520c01d0cd44e0775f148f4609fdf297b64ef0
casept <davids.paskevics@gmail.com>
2021-09-06 20:00:52 +0200

    [ccboot] More floppy trial-and-error

--- ccboot/src/boot1.c
+++ ccboot/src/boot1.c
@@ -4,13 +4,27 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "floppy.h"
+
+extern struct elf64_header_t elf64_header_parse_and_validate(const uint8_t* data);
+
 /// Where the real mode code loaded our kernel's binary to (in low memory).
-const uint8_t* BOOT1_KERNEL_COPY = (const uint8_t*)0x0000F000;
+uint8_t* BOOT1_KERNEL_COPY = (uint8_t*)0x0000F000;
 
 /// The entrypoint from ASM.
 void boot1_cmain(void) {
     vga_clear();
 
+    vga_printf("boot1: Reading kernel from floppy\n");
+    // First 18 sectors (first track) are reserved for bootloader,
+    // rest is kernel.
+    // For testing, reading the second track only is sufficient.
+    // TODO: Rewrite to read entire floppy
+    floppy_read(18, 18, BOOT1_KERNEL_COPY);
+    vga_printf("boot1: Kernel has been read\n");
+
+    vga_printf("boot1: Parsing and validating kernel ELF\n");
+    elf_register_fatalf(vga_vprintf);
     struct elf64_header_t hdr = elf64_header_parse_and_validate(BOOT1_KERNEL_COPY);
     (void)hdr;
     vga_printf("boot1: parsed and validated kernel ELF\n");
--- ccboot/src/floppy.c
+++ ccboot/src/floppy.c
@@ -21,12 +21,12 @@ enum floppy_ports_t {
 
 /// Commands that can be issued to the drive.
 enum floppy_commands_t {
-    FLOPPY_COMMAND_RECALIBRATE = 7,
-    FLOPPY_COMMAND_SENSE_INTERRUPT = 8,
-    FLOPPY_COMMAND_SEEK = 15,
-    FLOPPY_COMMAND_VERSION = 16,
-    FLOPPY_COMMAND_CONFIGURE = 19,
-    FLOPPY_COMMAND_LOCK = 148,
+    FLOPPY_COMMAND_RECALIBRATE = 0x07,
+    FLOPPY_COMMAND_SENSE_INTERRUPT = 0x08,
+    FLOPPY_COMMAND_SEEK = 0x0F,
+    FLOPPY_COMMAND_VERSION = 0x10,
+    FLOPPY_COMMAND_CONFIGURE = 0x13,
+    FLOPPY_COMMAND_LOCK = 0x94,
 };
 
 /// Bits making up the main status register
@@ -47,36 +47,46 @@ enum floppy_dor_bits_t {
     FLOPPY_DOR_BIT_IRQ = 0x08,
 };
 
+/* Pre-declaration of functions that have circular dependency on each other being declared */
+static int send_command(enum floppy_commands_t cmd, const uint8_t* param_bytes, size_t num_param_bytes,
+                        uint8_t* result_bytes, size_t num_result_bytes, size_t num_retries);
+static uint8_t cmd_sense_interrupt(void);
+static void cmd_configure(void);
+
 /// Reset the floppy controller.
-static void floppy_controller_cmd_reset(void) {
-    // Cannot use the `floppy_controller_send_command` function because it requires this function for retries, creating
-    // a circular dependency.
+static void cmd_reset(void) {
     port_write_u8(FLOPPY_PORT_DOR, 0x00);
-    // TODO: Real hardware requires 4us wait here
-    const uint8_t dor = FLOPPY_DOR_BIT_RESET;
-    port_write_u8(FLOPPY_PORT_DOR, dor);
+    port_write_u8(FLOPPY_PORT_DOR, 0x0C);
 
-    // TODO: Because we're in polling mode, send 4 sense interrupt commands
+    // Because we're in polling mode, send 4 sense interrupt commands
+    for (size_t i = 0; i < 4; i++) {
+        cmd_sense_interrupt();
+    }
 
-    // TODO: Re-send configure commands
+    // Re-send configuration
+    cmd_configure();
 }
 
-static uint8_t floppy_lba_2_cylinder(size_t lba) { return lba / (2 * FLOPPY_SECTORS_PER_TRACK); }
+static uint8_t lba_2_cylinder(size_t lba) { return lba / (2 * FLOPPY_SECTORS_PER_TRACK); }
 
-static uint8_t floppy_lba_2_head(size_t lba) {
-    return ((lba % (2 * FLOPPY_SECTORS_PER_TRACK)) / FLOPPY_SECTORS_PER_TRACK);
-}
+static uint8_t lba_2_head(size_t lba) { return ((lba % (2 * FLOPPY_SECTORS_PER_TRACK)) / FLOPPY_SECTORS_PER_TRACK); }
 
-static uint8_t floppy_lba_2_sector(size_t lba) {
+static uint8_t lba_2_sector(size_t lba) {
     return ((lba % (2 * FLOPPY_SECTORS_PER_TRACK)) % FLOPPY_SECTORS_PER_TRACK + 1);
 }
 
+static void wait_drive_inactive(void) {
+    while ((port_read_u8(FLOPPY_PORT_MSR) & (1 << FLOPPY_DRIVE_NUM)) == 0) {
+    }
+}
+
 /// Send a command or parameter byte to the floppy controller.
-static int floppy_controller_send_byte(uint8_t byte) {
+static int send_byte(uint8_t byte) {
     // Send command and wait for ack
     port_write_u8(FLOPPY_PORT_DATA_FIFO, byte);
     while ((port_read_u8(FLOPPY_PORT_MSR) & FLOPPY_MSR_BIT_RQM) == 0) {
-    }
+    };
+
     // Must verify that DIO == 0
     if ((port_read_u8(FLOPPY_PORT_MSR) & FLOPPY_MSR_BIT_DIO) != 0) {
         return -1;
@@ -96,51 +106,51 @@ static int floppy_controller_send_byte(uint8_t byte) {
 /// Command will be retried as indicated by `num_retries`.
 /// If the final retry also fails, returns a negative error code.
 /// Otherwise, returns `0`.
-static int floppy_controller_send_command(enum floppy_commands_t cmd, const uint8_t* param_bytes,
-                                          size_t num_param_bytes, uint8_t* result_bytes, size_t num_result_bytes,
-                                          size_t num_retries) {
+static int send_command(enum floppy_commands_t cmd, const uint8_t* param_bytes, size_t num_param_bytes,
+                        uint8_t* result_bytes, size_t num_result_bytes, size_t num_retries) {
     int err = 0;
     for (size_t i = 0; i <= num_retries; i++) {
         // Check whether previous command didn't receive a fatal error
         const uint8_t status = port_read_u8(FLOPPY_PORT_MSR);
-        vga_printf("MSR: 0b%b\n", (unsigned int)status);
         if ((status & FLOPPY_MSR_BIT_RQM) == 0) {
             err = -1;
-            floppy_controller_cmd_reset();
+            cmd_reset();
             continue;
         }
         if ((status & FLOPPY_MSR_BIT_DIO) != 0) {
             err = -2;
-            floppy_controller_cmd_reset();
+            cmd_reset();
             continue;
         }
 
         // Send command byte
-        if (floppy_controller_send_byte(cmd) == -1) {
+        if (send_byte(cmd) == -1) {
             err = -3;
-            floppy_controller_cmd_reset();
+            cmd_reset();
             continue;
         }
 
         // Send parameter bytes
         if (param_bytes != NULL) {
             for (size_t j = 0; j < num_param_bytes; j++) {
-                if (floppy_controller_send_byte(param_bytes[j]) == -1) {
-                    // TODO: Continue outer loop
-                    floppy_controller_cmd_reset();
-                    return -4;
+                if (send_byte(param_bytes[j]) == -1) {
+                    err = -4;
+                    cmd_reset();
+                    goto continue_retry_loop;
                 }
             }
         }
 
         // Wait for execution phase to end and result phase to begin
+        if ((port_read_u8(FLOPPY_PORT_MSR) & FLOPPY_MSR_BIT_NDMA) == 0) {
+            // No execution phase; proceed directly to result phase
+            break;
+        }
         while (true) {
-            const uint8_t exec_status = port_read_u8(FLOPPY_PORT_MSR);
-            if ((exec_status & FLOPPY_MSR_BIT_NDMA) != 0) {
-                // No execution phase; proceed directly to result
+            if ((port_read_u8(FLOPPY_PORT_MSR) & FLOPPY_MSR_BIT_RQM) != 0) {
+                // TODO: Make work for commands which requires data transfer during execution phase
                 break;
             }
-            // TODO: Other waiting
         }
 
         // Read result bytes
@@ -155,55 +165,65 @@ static int floppy_controller_send_command(enum floppy_commands_t cmd, const uint
         const uint8_t end_status = port_read_u8(FLOPPY_PORT_MSR);
         if ((end_status & FLOPPY_MSR_BIT_RQM) == 0) {
             err = -5;
-            floppy_controller_cmd_reset();
+            cmd_reset();
             continue;
         }
         if ((end_status & FLOPPY_MSR_BIT_CB) != 0) {
             err = -6;
-            floppy_controller_cmd_reset();
+            cmd_reset();
             continue;
         }
         if ((end_status & FLOPPY_MSR_BIT_DIO) != 0) {
             err = -7;
-            floppy_controller_cmd_reset();
+            cmd_reset();
             continue;
         }
 
         return 0;
+    continue_retry_loop:;
     }
 
     return err;
 }
 
-static void floppy_controller_cmd_configure(void) {
-    // Implied seek enabled, threshold 8
+static void cmd_configure(void) {
+    // Implied seek enabled, FIFO threshold 8, IRQ and DMA disabled
     const uint8_t params[3] = {0x00, (1 << 6) | 8, 0x00};
-    floppy_controller_send_command(FLOPPY_COMMAND_CONFIGURE, params, 3, NULL, 0, 5);
-}
-
-static void floppy_controller_cmd_lock_config(void) {
-    uint8_t result;
-    floppy_controller_send_command(FLOPPY_COMMAND_LOCK, NULL, 0, &result, 1, 5);
-}
-
-static void floppy_controller_cmd_recalibrate(void) {
-    const uint8_t params[1] = {FLOPPY_DRIVE_NUM};
-    floppy_controller_send_command(FLOPPY_COMMAND_RECALIBRATE, params, 1, NULL, 0, 5);
-
-    // Spin until drives no longer busy
-    while (true) {
-        const uint8_t msr = port_read_u8(FLOPPY_PORT_MSR);
-        if ((msr & (FLOPPY_MSR_BIT_ACTA | FLOPPY_MSR_BIT_ACTB | FLOPPY_MSR_BIT_ACTC | FLOPPY_MSR_BIT_ACTD)) == 0) {
-            break;
-        }
+    const int err = send_command(FLOPPY_COMMAND_CONFIGURE, params, 3, NULL, 0, 5);
+    if (err != 0) {
+        vga_fatalf("could not read floppy: failed to configure controller (error code: %d)", err);
     }
+}
+
+static void cmd_lock_config(void) {
+    uint8_t result;
+    const int err = send_command(FLOPPY_COMMAND_LOCK, NULL, 0, &result, 1, 5);
+    if (err != 0) {
+        vga_fatalf("could not read floppy: failed to lock controller configuration (error code: %d)", err);
+    }
+}
+
+static void cmd_recalibrate(void) {
+    const uint8_t params[1] = {FLOPPY_DRIVE_NUM};
+    const int err = send_command(FLOPPY_COMMAND_RECALIBRATE, params, 1, NULL, 0, 5);
+    if (err != 0) {
+        vga_fatalf("could not read floppy: failed to recalibrate controller (error code: %d)", err);
+    }
+
+    wait_drive_inactive();
     // TODO: Sense interrupt
 }
 
-static void floppy_controller_init(void) {
+static void init(void) {
+    vga_printf("reset\n");
+    cmd_reset();
+    vga_printf("configure\n");
+    cmd_configure();
+    vga_printf("lock_config\n");
+    cmd_lock_config();
     // Ensure controller version is not pre-prehistoric or from the future
     uint8_t version;
-    const int err = floppy_controller_send_command(FLOPPY_COMMAND_VERSION, NULL, 0, &version, 1, 5);
+    const int err = send_command(FLOPPY_COMMAND_VERSION, NULL, 0, &version, 1, 5);
     if (err != 0) {
         vga_fatalf("could not read floppy: command to determine controller version failed (error code: %d)", err);
     }
@@ -212,55 +232,44 @@ static void floppy_controller_init(void) {
                    (unsigned int)FLOPPY_CONTROLLER_SUPPORTED_VERSION, (unsigned int)version);
     }
 
-    // Send configuration
-    floppy_controller_cmd_configure();
-
-    // Lock it in so it survives controller resets
-    floppy_controller_cmd_lock_config();
-
-    // Actual reset
-    floppy_controller_cmd_reset();
-
-    // Recalibrate the drive
-    floppy_controller_cmd_recalibrate();
-}
-
-static void floppy_controller_wait_drive_inactive(void) {
-    const uint8_t msr = port_read_u8(FLOPPY_PORT_MSR);
-    while ((msr & (1 << FLOPPY_DRIVE_NUM)) == 0) {
-    }
+    cmd_configure();
+    cmd_lock_config();
+    cmd_reset();
+    cmd_recalibrate();
 }
 
 /// Sends a sense interrupt command to the drive.
 ///
 /// Returns st0.
-static uint8_t floppy_controller_cmd_sense_interrupt(void) {
+static uint8_t cmd_sense_interrupt(void) {
     uint8_t result[2];
-    if (floppy_controller_send_command(FLOPPY_COMMAND_SENSE_INTERRUPT, NULL, 0, result, 2, 5) == -1) {
-        vga_fatalf("could not read floppy: failed to send sense interrupt");
+    const int err = send_command(FLOPPY_COMMAND_SENSE_INTERRUPT, NULL, 0, result, 2, 5);
+    if (err != 0) {
+        vga_fatalf("could not read floppy: failed to send sense interrupt (error code: %d)", err);
     }
     return result[0];
 }
 
-static void floppy_controller_cmd_seek(size_t lba) {
-    const uint8_t c = floppy_lba_2_cylinder(lba);
-    const uint8_t h = floppy_lba_2_head(lba);
+static void cmd_seek(size_t lba) {
+    const uint8_t c = lba_2_cylinder(lba);
+    const uint8_t h = lba_2_head(lba);
     const uint8_t params[2] = {(h << 2) | FLOPPY_DRIVE_NUM, c};
-    if (floppy_controller_send_command(FLOPPY_COMMAND_SEEK, params, 2, NULL, 0, 5) == -1) {
-        vga_fatalf("could not read floppy: failed to submit seek command");
+    const int err = send_command(FLOPPY_COMMAND_SEEK, params, 2, NULL, 0, 5);
+    if (err != 0) {
+        vga_fatalf("could not read floppy: failed to submit seek command (error code: %d)", err);
     }
     // Head movement is done once drive is inactive
-    floppy_controller_wait_drive_inactive();
+    wait_drive_inactive();
     // Check for errors
-    const uint8_t st0 = floppy_controller_cmd_sense_interrupt();
+    const uint8_t st0 = cmd_sense_interrupt();
     if ((st0 & 0xC0) > 0) {
         vga_fatalf("could not read floppy: seek command reported failure");
     }
 }
 
-static void floppy_controller_read(size_t lba, uint8_t* dest) {
+static void read_sector(size_t lba, uint8_t* dest) {
     // Seek to correct cylinder
-    floppy_controller_cmd_seek(lba);
+    cmd_seek(lba);
     // TODO: Issue read command
     // TODO: Poll until done
     // TODO: Check for errors and retry
@@ -268,10 +277,11 @@ static void floppy_controller_read(size_t lba, uint8_t* dest) {
 }
 
 void floppy_read(size_t lba_start_sector_idx, size_t num_sectors, uint8_t* dest) {
-    floppy_controller_init();
+    init();
     for (size_t i = 0; i < num_sectors; i++) {
-        floppy_controller_read(lba_start_sector_idx + i, dest);
+        read_sector(lba_start_sector_idx + i, dest);
         dest += FLOPPY_SECTOR_SIZE;
     }
-    (void)floppy_lba_2_sector;
+    // TODO: Use
+    (void)lba_2_sector;
 }
