8975bd3f9acbc9fd2e15f6b5065b8ade1a4a0ba1
casept <davids.paskevics@gmail.com>
2021-09-08 21:35:01 +0200

    [toolchain] Fix toolchain flags for kernel builds

--- toolchains/i686-unknown-elf-gcc.mk
+++ toolchains/i686-unknown-elf-gcc.mk
@@ -8,5 +8,5 @@ LD = $(TC_PATH)/$(TC_PREFIX)ld
 AR = $(TC_PATH)/$(TC_PREFIX)ar
 OBJCOPY = $(TC_PATH)/$(TC_PREFIX)objcopy
 
-CFLAGS = -nostdlib -ffreestanding -lgcc -Wall -Wextra -Wpedantic -Werror -Wcast-align -Wcast-qual -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wredundant-decls -Wshadow -Wsign-conversion -Wstrict-overflow=5 -Wswitch-default -Wundef -Wunreachable-code -Wwrite-strings -fdiagnostics-color=always
+CFLAGS = -nostdlib -ffreestanding -static-libgcc -lgcc -Wall -Wextra -Wpedantic -Werror -Wcast-align -Wcast-qual -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wredundant-decls -Wshadow -Wsign-conversion -Wstrict-overflow=5 -Wswitch-default -Wundef -Wunreachable-code -Wwrite-strings -fdiagnostics-color=always
 CXXFLAGS += -nostdlib -ffreestanding -lgcc -fno-exceptions -fno-rtti -fno-unwind-tables -fno-use-cxa-atexit -fno-threadsafe-statics -Wall -Wextra -Wpedantic -Werror -Wcast-align -Wcast-qual -Wdisabled-optimization -Wformat=2 -Winit-self -Wlogical-op -Wredundant-decls -Wshadow -Wsign-conversion -Wstrict-overflow=5 -Wswitch-default -Wundef -Wunreachable-code -Wwrite-strings -Wctor-dtor-privacy -Wold-style-cast -Wstrict-null-sentinel -Woverloaded-virtual -Wsign-promo -Wnoexcept -Wzero-as-null-pointer-constant -Wuseless-cast -fdiagnostics-color=always
