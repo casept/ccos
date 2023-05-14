f128a13e5fb759dea9baadd4d53a6bf64cd6b2c5
casept <davids.paskevics@gmail.com>
2023-05-14 11:39:53 +0200

    [cccore]: Fix garbled debug output caused by using va_list twice in kprintf

--- cccore/src/common.c
+++ cccore/src/common.c
@@ -12,23 +12,29 @@ void kprint_init(void) {
 }
 
 void kprintf(const char* format, ...) {
-    va_list vlist;
-    va_start(vlist, format);
-    vga_vprintf(format, vlist);
-    serial_com1_vprintf(format, vlist);
-    va_end(vlist);
+    va_list vlist_serial;
+    va_start(vlist_serial, format);
+    va_list vlist_vga;
+    va_copy(vlist_vga, vlist_serial);
+    serial_com1_vprintf(format, vlist_serial);
+    vga_vprintf(format, vlist_vga);
+    va_end(vlist_vga);
+    va_end(vlist_serial);
 }
 
 void __attribute__((noreturn)) kpanicf(const char* format, ...) {
     vga_printf("PANIC: ");
     serial_com1_printf("PANIC: ");
-    va_list vlist;
-    va_start(vlist, format);
-    vga_vprintf(format, vlist);
-    serial_com1_vprintf(format, vlist);
+    va_list vlist_serial;
+    va_start(vlist_serial, format);
+    va_list vlist_vga;
+    va_copy(vlist_vga, vlist_serial);
+    serial_com1_vprintf(format, vlist_serial);
+    vga_vprintf(format, vlist_serial);
     __asm__ volatile("cli; hlt");
     // To make the stupid compiler happy
-    va_end(vlist);
+    va_end(vlist_vga);
+    va_end(vlist_serial);
     while (true) {
     }
 }
